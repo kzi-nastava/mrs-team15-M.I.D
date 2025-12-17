da3ddf9cfcff6e56a38f32ddf4101a7e3c7977b2
MiaUgljesic <miaugljesic@gmail.com>
2025-12-17 01:13:32 +0100

    feat(S1.2.3.4): add changing password ui

--- frontend/src/app/auth/components/profile-form/profile-form.ts
+++ frontend/src/app/auth/components/profile-form/profile-form.ts
@@ -1,12 +1,13 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { InputComponent } from '../../../shared/components/input-component/input-component';
 import { Button } from '../../../shared/components/button/button';
-import { VehicleForm } from '../vehicle-form/vehicle-form'; // <-- dodaj ovo
+import { VehicleForm } from '../vehicle-form/vehicle-form'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-form',
   standalone: true,
-  imports: [Button, InputComponent, VehicleForm], // <-- dodaj ovde
+  imports: [Button, InputComponent, VehicleForm], 
   templateUrl: './profile-form.html',
   styleUrl: './profile-form.css',
 })
@@ -31,25 +32,26 @@ export class ProfileForm {
     }
   };
 
-  // Klik na sliku otvara file picker
   onSelectPhoto(): void {
     this.fileInput.nativeElement.click();
   }
 
-  // File je izabran, ali trenutno ništa ne radimo sa njim
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       console.log('File selected:', input.files[0].name);
-      // trenutno ne radimo ništa sa slikom
     }
   }
 
   onUploadPhoto(): void {
     console.log('Upload photo clicked');
   }
-
+  constructor(private router: Router) {}
+  goToChangePassword() {
+  this.router.navigate(['/change-password']);
+    }
   onSave(): void {
-    console.log('Save profile', this.user);
-  }
+  console.log('Save profile', this.user);
+  this.router.navigate(['/landing']);       
+}
 }
--- frontend/src/app/pages/change-password/change-password.spec.ts
+++ frontend/src/app/pages/change-password/change-password.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormSelect } from './form-select';
 
-describe('FormSelect', () => {
-  let component: FormSelect;
-  let fixture: ComponentFixture<FormSelect>;
+import { ChangePassword } from './change-password';
+
+describe('ChangePassword', () => {
+  let component: ChangePassword;
+  let fixture: ComponentFixture<ChangePassword>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormSelect],
-    }).compileComponents();
+      imports: [ChangePassword]
+    })
+    .compileComponents();
 
-    fixture = TestBed.createComponent(FormSelect);
+    fixture = TestBed.createComponent(ChangePassword);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
