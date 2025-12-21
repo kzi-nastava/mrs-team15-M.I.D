244b771d6b25c3189a221c0cb12fd4b1ad09868e
dejana šević <dejana.sevic24@gmail.com>
2025-12-22 00:27:17 +0100

    feat(S3.2.2.1.2): add reset password endpoint

--- backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/controller/AuthController.java
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.asd.ridenow.dto.auth.ForgotPasswordRequestDTO;
 import rs.ac.uns.ftn.asd.ridenow.dto.auth.LoginRequestDTO;
 import rs.ac.uns.ftn.asd.ridenow.dto.auth.LoginResponseDTO;
+import rs.ac.uns.ftn.asd.ridenow.dto.auth.ResetPasswordRequestDTO;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -32,6 +33,18 @@ public class AuthController {
     @PostMapping("/forgot-password")
     public ResponseEntity<Void> forgotPassword(@RequestBody ForgotPasswordRequestDTO request){
         if(request.getEmail() == null || request.getEmail().isEmpty()){
+            return ResponseEntity.status(400).build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Void> resetPassword(@RequestBody ResetPasswordRequestDTO request){
+        if (request.getNewPassword() == null || request.getConfirmNewPassword() == null
+                || request.getNewPassword().isEmpty() || request.getConfirmNewPassword().isEmpty()){
+            return ResponseEntity.status(400).build();
+        }
+        if(!request.getNewPassword().equals(request.getConfirmNewPassword())){
             return ResponseEntity.status(400).build();
         }
         return ResponseEntity.ok().build();
