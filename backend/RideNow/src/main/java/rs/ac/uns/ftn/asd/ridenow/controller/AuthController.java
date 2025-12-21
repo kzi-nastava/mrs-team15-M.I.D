fe29980df89dcea13dae3dfb81ec9cb667731d4e
dejana šević <dejana.sevic24@gmail.com>
2025-12-21 22:51:53 +0100

    feat(S3.2.2.1.1): add login endpoint

---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/controller/AuthController.java
@@ -0,0 +1,24 @@
+package rs.ac.uns.ftn.asd.ridenow.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.uns.ftn.asd.ridenow.dto.auth.LoginRequestDTO;
+import rs.ac.uns.ftn.asd.ridenow.dto.auth.LoginResponseDTO;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login (@RequestBody LoginRequestDTO request){
+        if(!request.getEmail().contains("@")){
+            return ResponseEntity.status(401).build();
+        }
+        LoginResponseDTO response = new LoginResponseDTO();
+        response.setActive(true);
+        response.setEmail(request.getEmail());
+        response.setFirstName("Jane");
+        response.setLastName("Doe");
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/dto/auth/LoginRequestDTO.java
@@ -0,0 +1,26 @@
+package rs.ac.uns.ftn.asd.ridenow.dto.auth;
+
+public class LoginRequestDTO {
+    private String email;
+    private String password;
+
+    public LoginRequestDTO() {
+        super();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
