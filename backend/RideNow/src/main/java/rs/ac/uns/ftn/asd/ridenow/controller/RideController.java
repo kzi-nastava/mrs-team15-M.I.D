d7f900b4010d8b75955373fbe903c75472afd29c
dejana šević <dejana.sevic24@gmail.com>
2025-12-22 19:09:50 +0100

    feat(S3.2.6.5): add stop ride endpoint

--- backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/controller/RideController.java
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/controller/RideController.java
@@ -1,11 +1,9 @@
 package rs.ac.uns.ftn.asd.ridenow.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.asd.ridenow.dto.ride.RideEstimateResponseDTO;
+import rs.ac.uns.ftn.asd.ridenow.dto.ride.StopRideResponseDTO;
 
 @RestController
 @RequestMapping("/api/rides")
@@ -17,4 +15,13 @@ public class RideController {
         response.setEstimatedDurationMin(24);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/{id}/stop")
+    public ResponseEntity<StopRideResponseDTO> stop (@PathVariable Long id){
+        StopRideResponseDTO response = new StopRideResponseDTO();
+        response.setEndLocation("Bulevar Oslobodjenja 24, Novi Sad");
+        response.setPrice(570);
+        return  ResponseEntity.ok(response);
+    }
+
 }
