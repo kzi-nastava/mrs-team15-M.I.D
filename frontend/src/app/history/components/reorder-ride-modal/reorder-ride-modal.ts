cce921db151f213004c20296915983837d97435b
dejana šević <dejana.sevic24@gmail.com>
2026-01-05 18:32:14 +0100

    feat(S3.2.9.3): add admin history page with ride detatils #62

---
+++ frontend/src/app/history/components/reorder-ride-modal/reorder-ride-modal.ts
@@ -0,0 +1,51 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Button } from '../../../shared/components/button/button';
+import { Ride } from '../admin-history-table/admin-history-table';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reorder-ride-modal',
+  standalone: true,
+  imports: [Button, CommonModule, FormsModule],
+  templateUrl: './reorder-ride-modal.html',
+  styleUrl: './reorder-ride-modal.css',
+})
+
+export class ReorderRideModal {
+
+  @Input() ride!: Ride;
+  @Output() bookNow = new EventEmitter<void>();
+  @Output() schedule = new EventEmitter<{ date: string; time: string }>();
+  @Output() close = new EventEmitter<void>();
+
+  showScheduleInputs = false;
+
+  date = '';
+  time = '';
+
+  onClose(): void {
+    this.close.emit();
+  }
+
+  onBookNow(): void {
+    this.bookNow.emit();
+    this.close.emit();
+  }
+
+  onScheduleClick(): void {
+    this.showScheduleInputs = true;
+  }
+
+  onConfirmSchedule(): void {
+    this.schedule.emit({
+      date: this.date,
+      time: this.time
+    });
+    this.close.emit();
+  }
+
+  onScheduleForLater() {
+    this.showScheduleInputs = true;
+  }
+}
