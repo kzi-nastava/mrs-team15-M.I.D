ac4a2f78960f28fe3cd2f8656d9aa137af028a24
dejana šević <dejana.sevic24@gmail.com>
2026-01-08 20:55:51 +0100

    feat(S3.2.1.2): add ride estimation form validation #70

--- frontend/src/app/ride/components/ride-estimation-form/ride-estimation-form.ts
+++ frontend/src/app/ride/components/ride-estimation-form/ride-estimation-form.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { Button } from '../../../shared/components/button/button';
 import { InputComponent } from '../../../shared/components/input-component/input-component';
+import { FromValidator } from '../../../shared/components/form-validator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ride-estimation-form',
-  imports: [Button, InputComponent],
+  imports: [Button, InputComponent, CommonModule],
   templateUrl: './ride-estimation-form.html',
   styleUrl: './ride-estimation-form.css',
 })
 export class RideEstimationForm {
+  pickupAddress : string = '';
+  destinationAddress : string = '';
+
+  validator : FromValidator = new FromValidator();
+
+  hasErrors() : boolean {
+    return !!(this.validator.addressError(this.pickupAddress) || this.validator.addressError(this.destinationAddress));
+  }
 
 }
