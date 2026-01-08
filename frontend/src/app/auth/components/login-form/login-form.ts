56c7f06c4cfde487348bf4fa9b8ecbca1eb230fe
dejana šević <dejana.sevic24@gmail.com>
2026-01-08 12:10:34 +0100

    feat(S3.2.2.1): add login form validation #70

--- frontend/src/app/auth/components/login-form/login-form.ts
+++ frontend/src/app/auth/components/login-form/login-form.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Button } from '../../../shared/components/button/button';
 import { InputComponent } from '../../../shared/components/input-component/input-component'
 import { RouterLink } from '@angular/router'
+import { FromValidator } from '../../../shared/components/form-validator';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [Button, InputComponent, RouterLink],
+  imports: [Button, InputComponent, RouterLink, CommonModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
@@ -19,5 +21,13 @@ export class LoginForm {
     if(input) {
       input.type = this.passwordVisible ? 'text' : 'password';
     }
+  }
+
+  email: string = '';
+  password: string = '';
+  validator : FromValidator = new FromValidator()
+
+  hasErrors() : boolean{
+    return this.validator.isEmailValid(this.email) === false || this.validator.isPasswordValid(this.password) === false;
   }
 }
