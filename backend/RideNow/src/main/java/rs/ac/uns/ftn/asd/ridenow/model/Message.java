59fca87bc94477a2589602091e4639edbaf50365
dejana šević <dejana.sevic24@gmail.com>
2026-01-09 22:03:08 +0100

    feature: add Message class #86

---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/Message.java
@@ -0,0 +1,38 @@
+package rs.ac.uns.ftn.asd.ridenow.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Lob
+    private String message;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+
+    public Message(Long id, String message, LocalDateTime timestamp) {
+        this.id = id;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public Message() {
+    }
+}
