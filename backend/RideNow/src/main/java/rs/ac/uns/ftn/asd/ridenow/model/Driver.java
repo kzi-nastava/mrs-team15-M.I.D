a3df491ac45a71f41eebac6eca1c16d3f7d1f320
dejana šević <dejana.sevic24@gmail.com>
2026-01-09 22:55:09 +0100

    refactor: add JPA constraints to Driver class #86

--- backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/Driver.java
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/Driver.java
@@ -1,35 +1,39 @@
 package rs.ac.uns.ftn.asd.ridenow.model;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.Setter;
 import rs.ac.uns.ftn.asd.ridenow.model.enums.DriverStatus;
 
-@Getter @Setter
+@Getter
+@Setter
 @Entity
-@DiscriminatorValue("DRIVER")
 public class Driver extends User {
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private DriverStatus status;
+
     @Column(nullable = false)
     private boolean available;
-    private double workingHoursLast24;
-    private double rating;
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "vehicle_id")
-    private Vehicle vehicle;
 
-    public Driver(String email, String password, String firstName, String lastName, String phoneNumber, String address,Long id,
+    @Column(nullable = false)
+    private double workingHoursLast24 = 0;
+
+    private double rating = 0.0;
+
+    public Driver(String email, String password, String firstName, String lastName, String phoneNumber, String address,
                   String profileImage, boolean active, boolean blocked, DriverStatus status, boolean available,
-                  double workingHoursLast24, double rating, Vehicle vehicle) {
-        super(email, password, firstName, lastName, phoneNumber, address,id, profileImage, active, blocked);
+                  double workingHoursLast24, double rating) {
+        super(email, password, firstName, lastName, phoneNumber, address, profileImage, active, blocked);
         this.status = status;
         this.available = available;
         this.workingHoursLast24 = workingHoursLast24;
         this.rating = rating;
-        this.vehicle = vehicle;
     }
 
     public Driver() {
-
+        super();
     }
 }
