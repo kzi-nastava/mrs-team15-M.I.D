a40524d8367bd5681414198096a0d54fea0d63c3
iigor000 <igor.novakovic2004@gmail.com>
2026-01-09 13:01:31 +0100

    Added form for reporting inconsistency

--- frontend/src/app/ride/components/current-ride-form/current-ride-form.ts
+++ frontend/src/app/ride/components/current-ride-form/current-ride-form.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Button } from '../../../shared/components/button/button';
 import { CommonModule } from '@angular/common';
+import { ReportInconsistencyModal } from '../report-inconsistency-modal/report-inconsistency-modal';
+
 @Component({
   selector: 'app-current-ride-form',
-  imports: [Button, CommonModule],
+  imports: [Button, CommonModule, ReportInconsistencyModal],
   templateUrl: './current-ride-form.html',
   styleUrl: './current-ride-form.css',
 })
 export class CurrentRideForm {
-  isDriver : boolean = true;
-  isPassenger : boolean = false;
+  @ViewChild(ReportInconsistencyModal) reportModal!: ReportInconsistencyModal;
+
+  isDriver : boolean = false;
+  isPassenger : boolean = true;
+
+  openReportModal() {
+    this.reportModal.openModal();
+  }
+
+  handleReportSubmitted(message: string) {
+    // Handle the report submission here
+    console.log('Report submitted:', message);
+    // You can add your API call here
+  }
 }
