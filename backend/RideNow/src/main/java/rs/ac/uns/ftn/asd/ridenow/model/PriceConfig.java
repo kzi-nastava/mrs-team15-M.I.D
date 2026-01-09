c45ab25921fd42ca6db0f8e88b7ca17a7ebe93f4
dejana šević <dejana.sevic24@gmail.com>
2026-01-09 23:58:35 +0100

    feature: add PriceConfig class #86

---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/PriceConfig.java
@@ -0,0 +1,35 @@
+package rs.ac.uns.ftn.asd.ridenow.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import rs.ac.uns.ftn.asd.ridenow.model.enums.NotificationType;
+import rs.ac.uns.ftn.asd.ridenow.model.enums.VehicleType;
+
+@Setter
+@Getter
+@Entity
+public class PriceConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private VehicleType vehicleType;
+
+    @Column(nullable = false)
+    private double basePrice;
+
+    @Column(nullable = false)
+    private double pricePerKm;
+
+    public PriceConfig(VehicleType vehicleType, double basePrice, double pricePerKm) {
+        this.vehicleType = vehicleType;
+        this.basePrice = basePrice;
+        this.pricePerKm = pricePerKm;
+    }
+
+    public PriceConfig() {
+    }
+}
