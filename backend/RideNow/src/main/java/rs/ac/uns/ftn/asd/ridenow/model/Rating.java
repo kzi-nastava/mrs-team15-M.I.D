5d5d0bf6627f1d0b2856eb3ad82b86c9114e53b3
dejana šević <dejana.sevic24@gmail.com>
2026-01-09 23:50:02 +0100

    feature: add Rating class #86

---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/Rating.java
@@ -0,0 +1,39 @@
+package rs.ac.uns.ftn.asd.ridenow.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  int driverRating = 0;
+
+    @Column(nullable = false)
+    private  int vehicleRating = 0;
+
+    @Lob
+    private String comment;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Rating(int driverRating, int vehicleRating, String comment) {
+        this.driverRating = driverRating;
+        this.vehicleRating = vehicleRating;
+        this.comment = comment;
+    }
+
+    public Rating() {
+    }
+}
