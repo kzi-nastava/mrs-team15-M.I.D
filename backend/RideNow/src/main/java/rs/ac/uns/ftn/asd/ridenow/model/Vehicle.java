0b24c277d7d4d7e49e47de2d5cbd33435780f40a
dejana šević <dejana.sevic24@gmail.com>
2026-01-09 23:32:30 +0100

    feature: add Vehicle class #86

--- backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/Vehicle.java
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/model/Vehicle.java
@@ -1,34 +1,46 @@
 package rs.ac.uns.ftn.asd.ridenow.model;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import rs.ac.uns.ftn.asd.ridenow.model.enums.VehicleType;
 
-@Getter @Setter
+@Getter
+@Setter
 @Entity
-@Table(name = "vehicles")
 public class Vehicle {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(nullable = false, unique = true)
     private String licencePlate;
+
+    @Column(nullable = false)
+    private String model;
+
     private double lat;
+
     private double lon;
+
     @Column(nullable = false)
     private boolean available;
-    private double rating;
-    @Column(nullable = false)
-    private boolean petFriendly;
-    @Column(nullable = false)
-    private boolean childFriendly;
-    @Column(nullable = false)
-    private VehicleType type;
 
-    public Vehicle() {}
+    @Column(nullable = false)
+    private double rating = 0.0;
+
+    @Column(nullable = false)
+    private boolean petFriendly = false;
+
+    @Column(nullable = false)
+    private boolean childFriendly = false;
+
+    @Column(nullable = false)
+    private int seatCount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private VehicleType type;
 
     public Vehicle(String licencePlate, double lat, double lon, boolean available, double rating) {
         this.licencePlate = licencePlate;
@@ -36,5 +48,21 @@ public class Vehicle {
         this.lon = lon;
         this.available = available;
         this.rating = rating;
+    }
+
+    public Vehicle(String licencePlate, String model, boolean available, double rating,
+                   boolean petFriendly, boolean childFriendly, int seatCount, VehicleType type) {
+        this.licencePlate = licencePlate;
+        this.model = model;
+        this.available = available;
+        this.rating = rating;
+        this.petFriendly = petFriendly;
+        this.childFriendly = childFriendly;
+        this.seatCount = seatCount;
+        this.type = type;
+    }
+
+    public Vehicle() {
+
     }
 }
