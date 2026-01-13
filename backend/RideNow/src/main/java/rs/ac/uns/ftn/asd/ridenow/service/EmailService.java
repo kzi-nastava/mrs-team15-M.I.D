8881402bccbd194824f7ee7bcd0109abc59b4ba2
dejana šević <dejana.sevic24@gmail.com>
2026-01-13 21:07:31 +0100

    feat(S3.2.2.2): add email activation backend logic #100

---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/service/EmailService.java
@@ -0,0 +1,23 @@
+package rs.ac.uns.ftn.asd.ridenow.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import rs.ac.uns.ftn.asd.ridenow.model.ActivationToken;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    private String activationRoute = "http://localhost:4200/activate/";
+
+    public void sendActivationMail(String to, ActivationToken token) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(to);
+        mail.setSubject("Activate your RideNow account");
+        mail.setText("Click to activate:" + activationRoute + token.getToken());
+        mailSender.send(mail);
+    }
+}
