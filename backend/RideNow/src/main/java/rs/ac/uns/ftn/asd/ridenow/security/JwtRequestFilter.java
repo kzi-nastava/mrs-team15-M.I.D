b3ea1ec3a854c31e072b0c8b0528bbaed1beceef
dejana šević <dejana.sevic24@gmail.com>
2026-01-14 17:26:53 +0100

    feat(S3.2.2.1.1): add JwtRequestFilter class #98

---
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/security/JwtRequestFilter.java
@@ -0,0 +1,50 @@
+package rs.ac.uns.ftn.asd.ridenow.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import rs.ac.uns.ftn.asd.ridenow.model.User;
+import rs.ac.uns.ftn.asd.ridenow.repository.UserRepository;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        String email = null;
+        String token = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            if (jwtUtil.validateToken(token)) {
+                email = jwtUtil.extractEmail(token);
+            }
+        }
+
+        if (email != null) {
+            Optional<User> optionalUser = userRepository.findByEmail(email);
+            if(optionalUser.isEmpty()){
+                return;
+            }
+            User user = optionalUser.get();
+            request.setAttribute("loggedInUser", user);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
