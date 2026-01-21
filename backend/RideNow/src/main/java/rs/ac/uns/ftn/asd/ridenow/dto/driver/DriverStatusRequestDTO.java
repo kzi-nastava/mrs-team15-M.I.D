3559d04eec6bf24907df1c766b6ad7967b1c6b49
dejana šević <dejana.sevic24@gmail.com>
2026-01-22 00:46:13 +0100

    feat: add driver status dto validator

--- backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/dto/driver/DriverStatusRequestDTO.java
+++ backend/RideNow/src/main/java/rs/ac/uns/ftn/asd/ridenow/dto/driver/DriverStatusRequestDTO.java
@@ -1,5 +1,7 @@
 package rs.ac.uns.ftn.asd.ridenow.dto.driver;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import rs.ac.uns.ftn.asd.ridenow.model.enums.DriverStatus;
@@ -7,6 +9,9 @@ import rs.ac.uns.ftn.asd.ridenow.model.enums.DriverStatus;
 @Getter
 @Setter
 public class DriverStatusRequestDTO {
+
+    @NotNull(message = "Driver status is required")
     DriverStatus status;
+
     public DriverStatusRequestDTO(){super();}
 }
