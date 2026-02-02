1cdfe4c13132e12cfa7d0189777d61ca3551acc6
dejana šević <dejana.sevic24@gmail.com>
2026-02-02 10:39:23 +0100

    feat(S3.2.2.1.1): add android request and response dto for login #201

---
+++ mobile/app/src/main/java/com/example/ridenow/dto/auth/LoginResponseDTO.java
@@ -0,0 +1,51 @@
+package com.example.ridenow.dto.auth;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResponseDTO {
+    private String token;
+    private String role;
+    private long expiresAt;
+
+    @SerializedName("hasCurrentRide")
+    private boolean hasCurrentRide;
+
+    public LoginResponseDTO() {}
+
+    public LoginResponseDTO(String token, String role, long expiresAt, boolean hasCurrentRide) {
+        this.token = token;
+        this.role = role;
+        this.expiresAt = expiresAt;
+        this.hasCurrentRide = hasCurrentRide;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public long getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(long expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+    public boolean getHasCurrentRide() {
+        return hasCurrentRide;
+    }
+    public void setHasCurrentRide(boolean hasCurrentRide) {
+        this.hasCurrentRide = hasCurrentRide;
+    }
+}
