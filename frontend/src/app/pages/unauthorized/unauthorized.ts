dc3e14aa6f8f0fd64528d91cec85d3f4eff77adb
iigor000 <igor.novakovic2004@gmail.com>
2026-02-23 13:29:42 +0100

    Added auth guard

---
+++ frontend/src/app/pages/unauthorized/unauthorized.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Button } from '../../shared/components/button/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-unauthorized',
+  standalone: true,
+  imports: [Button],
+  templateUrl: './unauthorized.html',
+  styleUrl: './unauthorized.css',
+})
+export class Unauthorized {
+  constructor(private location: Location, private router: Router) {}
+
+  goBack(): void {
+    let role = localStorage.getItem('role');
+    if (role === 'ADMIN') {
+      this.router.navigate(['/admin-active-rides']);
+    } else if (role === 'DRIVER') {
+      this.router.navigate(['/upcoming-rides']);
+    } else if (role === 'USER') {
+      this.router.navigate(['/upcoming-rides']);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+}
